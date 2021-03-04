8170774826219debb8590407a78bb880d1b45e80
rtrunck <rtrunck@gmail.com>
2021-03-04 11:04:38 -0700

    Working class based solution.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  // Step 1: Constructor
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  // Step 3: implement incrememt function
+  increment () {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  // Step 4: implement decrement function
+  decrement () {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        {/* Step 6: call increment function */}
+        <button onClick={this.decrement}>-</button>
+        <h1>{/* Step 2: Display Count */ this.state.count}</h1>
+        {/* Step 5: call decrement function */}
+        <button onClick={this.increment}>+</button>
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
+export default Counter
