cb2a0b36015740dda53904f5df493cf48ae80830
rtrunck <rtrunck@gmail.com>
2021-03-04 11:05:48 -0700

    Class solution outline

--- src/App.js
+++ src/App.js
@@ -3,34 +3,19 @@ import './App.css'
 
 class Counter extends React.Component {
   // Step 1: Constructor
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      count: 0
-    }
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-  }
 
   // Step 3: implement incrememt function
-  increment () {
-    this.setState({ count: this.state.count + 1 })
-  }
 
   // Step 4: implement decrement function
-  decrement () {
-    this.setState({ count: this.state.count - 1 })
-  }
 
   render () {
     return (
       <React.Fragment>
         {/* Step 6: call increment function */}
-        <button onClick={this.decrement}>-</button>
-        <h1>{/* Step 2: Display Count */ this.state.count}</h1>
+        <button>-</button>
+        <h1>{/* Step 2: Display Count */}</h1>
         {/* Step 5: call decrement function */}
-        <button onClick={this.increment}>+</button>
+        <button>+</button>
       </React.Fragment>
     )
   }
